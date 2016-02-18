c280d94efd40ca7117ceb27f3420d643ce2ff9c4
Gaurav Pant <gauravpgaurav@gmail.com>
2016-02-18 12:09:22 +0530

    Removed unnecessary Bill object

--- DesignPatterns/src/bheav/pattern/startegy/Client.java
+++ DesignPatterns/src/bheav/pattern/startegy/Client.java
@@ -4,17 +4,16 @@ public class Client {
 
 	public static void main(String[] args) {
 
-		BillPayment bill1 = new BillPayment();
-		BillPayment bill2 = new BillPayment();
+		BillPayment bill = new BillPayment();
 
 		CardPayment cardPmt = new CardPayment();
 		CashPayment cashPmt = new CashPayment();
 
-		bill1.setStrategy(cardPmt);
-		System.out.println(bill1.makePayment(1000, "ICICI12345"));
+		bill.setStrategy(cardPmt);
+		System.out.println(bill.makePayment(1000, "ICICI12345"));
 		System.out.println();
-		bill2.setStrategy(cashPmt);
-		System.out.println(bill2.makePayment(1000, "101"));
+		bill.setStrategy(cashPmt);
+		System.out.println(bill.makePayment(1000, "101"));
 
 	}
 
